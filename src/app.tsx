d0c88e0c18512436765f1e68879011fc446406ba
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-05-08 22:56:22 +0200

    🎉 : init project

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./experience";
+
+export const App = () => {
+  return (
+    <>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [-4, 3, 6],
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </>
+  );
+};
