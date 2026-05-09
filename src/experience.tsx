aebffef1549c69377156efbb521c0496a797eabc
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-05-09 14:51:45 +0200

    ✨: integrate AccumulativeShadows and RandomizedLight for improved lighting effects in Experience component

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,24 +1,26 @@
-import { OrbitControls, useHelper } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  OrbitControls,
+  RandomizedLight,
+} from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
 import { useRef } from "react";
-import {
-  DirectionalLightHelper,
-  type DirectionalLight,
-  type Mesh,
-} from "three";
+import { type DirectionalLight, type Mesh } from "three";
 
 export const Experience = () => {
   const cube = useRef<Mesh>(null);
   const directionalLight = useRef<DirectionalLight>(null!);
 
-  useFrame((_state, delta) => {
+  useFrame((state, delta) => {
+    const time = state.clock.getElapsedTime();
     if (cube.current) {
       cube.current.rotation.y += delta * 0.2;
+      cube.current.position.x = Math.sin(time) + 2;
     }
   });
 
-  useHelper(directionalLight, DirectionalLightHelper, 1);
+  // useHelper(directionalLight, DirectionalLightHelper, 1);
 
   return (
     <>
@@ -33,8 +35,35 @@ export const Experience = () => {
         intensity={4.5}
         castShadow
         shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-left={-5}
+        shadow-camera-right={5}
+        shadow-camera-top={5}
+        shadow-camera-bottom={-5}
       />
       <ambientLight intensity={1.5} />
+
+      {/* <BakeShadows /> */}
+      {/* <SoftShadows size={25} samples={10} focus={0} /> */}
+      <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        opacity={0.8}
+        color="#316d39"
+        frames={Infinity}
+        temporal
+        blend={100}
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={3}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows>
 
       <mesh position-x={-2} castShadow>
         <sphereGeometry />
@@ -46,12 +75,7 @@ export const Experience = () => {
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-        receiveShadow
-      >
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
